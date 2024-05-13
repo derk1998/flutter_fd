f440046d99812d9d3e988e19760560e0195e141f
Derk Wiegerinck <derkwiegerinck@gmail.com>
2024-05-13 17:28:52 +0200

    Add stateful screen view state to interface

--- lib/flutter_fd.dart
+++ lib/flutter_fd.dart
@@ -6,7 +6,8 @@ export 'src/flow/flow_view_model.dart' show FlowScreenViewModel;
 export 'src/flow/flow_state.dart' show FlowState;
 
 export 'src/mvvm/screen.dart' show Screen;
-export 'src/mvvm/screen_view.dart' show StatefulScreenView, StatelessScreenView;
+export 'src/mvvm/screen_view.dart'
+    show StatefulScreenView, StatelessScreenView, StatefulScreenViewState;
 export 'src/mvvm/screen_view_model.dart'
     show ScreenViewModel, StatefulScreenViewModel;
 export 'src/navigation_manager.dart' show NavigationManager;
